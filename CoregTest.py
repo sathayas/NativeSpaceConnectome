61bc5d711b357d693330d8e1aa41f2eeeedd350d
Satoru Hayasaka <hayasaka@utexas.edu>
2019-06-25 15:03:06 -0500

    Now estimation and writing are two separate nodes; write node is a MapNode

--- CoregTest.py
+++ CoregTest.py
@@ -111,10 +111,15 @@ realign = Node(spm.Realign(),
                name="realign")
 
 
-# coregistration, T1w (native) to fMRI (native)
-coreg = Node(spm.Coregister(cost_function='nmi'),
-                name="coreg")
+# coregistration, estimation, T1w (native) to fMRI (native)
+coregEst = Node(spm.Coregister(cost_function='nmi', jobtype='estimate'),
+                name="coregEst")
 
+# coregistration, writing, T1w (native) to fMRI (native)
+coregWrite = MapNode(spm.Coregister(cost_function='nmi', jobtype='write'),
+                     name="coregWrite",
+                     iterfield=['apply_to_files'],
+                     nested=True)
 
 
 # creating a workflow
@@ -123,10 +128,11 @@ MNI = Workflow(name="MNI", base_dir=outDir)
 # connecting the nodes to the main workflow
 MNI.connect(extract, 'roi_file', realign, 'in_files')
 MNI.connect(gunzip_T1w, 'out_file', segNative, 'channel_files')
-MNI.connect(gunzip_T1w, 'out_file', coreg, 'source')
-MNI.connect(realign, 'mean_image', coreg, 'target')
-MNI.connect(segNative, 'native_class_images', coreg, 'apply_to_files')
-
+MNI.connect(gunzip_T1w, 'out_file', coregEst, 'source')
+MNI.connect(realign, 'mean_image', coregEst, 'target')
+MNI.connect(gunzip_T1w, 'out_file', coregWrite, 'source')
+MNI.connect(realign, 'mean_image', coregWrite, 'target')
+MNI.connect(segNative, 'native_class_images', coregWrite, 'apply_to_files')
 
 # running the workflow
 MNI.run()
