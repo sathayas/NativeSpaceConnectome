35656169e4e95743c480b3582b1f8a236ce06ed2
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-12 15:48:37 -0500

    kernel size is 1

--- CoregTest.py
+++ CoregTest.py
@@ -20,7 +20,7 @@ sites = ['Berlin_Margulies',
          'Queensland'
          ]
 # kernel size for erosion / dilation
-k_size = 0.5
+k_size = 1
 # Directory where resting-state raw data reside
 dataDir = '/home/satoru/Projects/Connectome/Data/1000FCP'
 #dataDir = '/Users/sh45474/Documents/Research/Project/NativeSpaceConnectome/Data'
